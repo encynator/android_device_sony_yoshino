d16e2dbc187e1a209681c20dc89b2414e8b6f1f3
Andreas Schneider <asn@cryptomilk.org>
2017-11-21 10:05:29 +0100

    config-files: Add config for POWER HAL

--- platform/config-files.mk
+++ platform/config-files.mk
@@ -20,3 +20,8 @@ PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(PLATFORM_PATH)/config/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(PLATFORM_PATH)/config/media/media_profiles_V1_0.xml:vendor/etc/media_profiles_V1_0.xml
+
+### POWER
+# RQBalance-PowerHAL configuration
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/power/rqbalance_config.xml:system/etc/rqbalance_config.xml
